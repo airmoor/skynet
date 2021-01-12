8bceadfd53d4d8ffd83341cb7c7e18a37f136503
ashatskaya <nastia.schatskaya@gmail.com>
2021-01-12 21:54:11 +0300

    upd steps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,84 @@
 <template>
 	<div class="home layout">
 		<div v-for="(step, idx) of data" :key="idx">
-			<h4 class="p-b-16">
-				{{step.title}}
-			</h4>
-			<div v-for="(variant, index) of step.variants"
-			     :key="index"
-			     class="home__block"
-			     :style="homeBlockColor(variant.color)">
-				<div class="flex justify-space-between p-b-16">
-					<h4>{{variant.title}}</h4>
-					<h5>{{variant.price_default}} ₽</h5>
-				</div>
+			<transition name="route" mode="out-in">
+				<div v-if="idx<=userStep">
+					<h4 class="p-b-16">
+						{{step.title}}
+					</h4>
+					<div v-for="(variant, index) of step.variants"
+					     :key="index"
+					     class="home__block"
+					     :style="homeBlockColor(variant.color)">
 
-				<div class="home__block__grid">
-					<div> {{variant.description}} </div>
-					<hr class="home__block__divider"/>
-					<div>
-						<div v-if="variant.options && variant.options.length" class="m-b-16">
-							<div v-for="option of variant.options" :key="option.title">
-								<div class="home__block__checkbox__block">
-									<label class="flex text-center fullwidth" :for="option.title">
-									  <span class="checkbox__input">
-									    <input type="checkbox"
-									           @change="calculate"
-									           class="home__block__checkbox"
-									           :value="option.price"
-									           v-model="option.isChecked"
-									           :id="option.title"
-									           name="checkbox">
-									    <span class="checkbox__control">
-									      <svg viewBox='0 0 24 24' aria-hidden="true" focusable="false">
-									        <path fill='none' stroke='currentColor' stroke-width='3'
-									              d='M1.73 12.91l6.37 6.37L22.79 4.59'/></svg>
-									    </span>
-									  </span>
-										<span class="fullwidth">{{option.title}}</span>
-									</label>
+						<div class="flex justify-space-between p-b-16">
+							<h4>{{variant.title}}</h4>
+							<h5>{{variant.price_default}} ₽</h5>
+						</div>
+
+						<div class="home__block__grid">
+							<div> {{variant.description}}</div>
+							<hr class="home__block__divider"/>
+							<div>
+								<div v-if="variant.options && variant.options.length" class="m-b-16">
+									<div v-for="option of variant.options" :key="option.title">
+										<div class="home__block__checkbox__block">
+											<label class="flex text-center fullwidth" :for="option.title">
+											  <span class="checkbox__input">
+											    <input type="checkbox"
+											           @change="calculate"
+											           class="home__block__checkbox"
+											           :value="option.price"
+											           v-model="option.isChecked"
+											           :id="option.title"
+											           name="checkbox">
+												    <span class="checkbox__control">
+												      <svg viewBox='0 0 24 24' aria-hidden="true" focusable="false">
+												        <path fill='none' stroke='currentColor' stroke-width='3'
+												              d='M1.73 12.91l6.37 6.37L22.79 4.59'/>
+												      </svg>
+												     </span>
+											    </span>
+												<span class="fullwidth">{{option.title}}</span>
+											</label>
+										</div>
+									</div>
 								</div>
+
+								<div v-if="variant.select && variant.select.length" class="m-b-16">
+									<form>
+										<div v-for="(select, selectIdx) of variant.select" :key="selectIdx">
+											<div class="m-b-16">{{select.title}}</div>
+
+											<select class="fullwidth home__block__selectbox" @change="calculate"
+											        v-model="select.select">
+												<option :value="option.price" class="fullwidth"
+												        v-for="(option, optionIdx) of select.items"
+												        :key="optionIdx">
+													{{option.title}}
+												</option>
+											</select>
+										</div>
+									</form>
+								</div>
+								<button class="fullwidth"
+								        :class="isTypeSelected(step.title, variant) ? 'button--primary' : 'button--default'"
+								        @click="selectType(step.title, variant)">
+									<h5 class="flex-center">
+										<span v-if="isTypeSelected(step.title, variant)">
+											Выбрано
+										</span>
+										<span v-else>
+											Выбрать
+										</span>
+									</h5>
+								</button>
 							</div>
 						</div>
-
-						<div v-if="variant.select && variant.select.length" class="m-b-16">
-							<form>
-								<div v-for="(select, selectIdx) of variant.select" :key="selectIdx" >
-									<div class="m-b-16">{{select.title}}</div>
-
-									<select class="fullwidth home__block__selectbox" @change="calculate"
-									        v-model="select.select"  >
-										<option :value="option.price" class="fullwidth"
-										        v-for="(option, optionIdx) of select.items"
-										        :key="optionIdx">
-											{{option.title}}
-										</option>
-									</select>
-								</div>
-							</form>
-						</div>
-						<button class="fullwidth"
-						        :class="isTypeSelected(step.title, variant) ? 'button--primary' : 'button--default'"
-						        @click="selectType(step.title, variant)">
-							<h5 class="flex-center">
-								<span v-if="isTypeSelected(step.title, variant)">Выбрано</span>
-								<span v-else>
-									Выбрать
-								</span>
-							</h5>
-						</button>
 					</div>
+					<hr class="m-y-16"/>
 				</div>
-			</div>
-
-			<hr class="m-y-16"/>
+			</transition>
 		</div>
 
 		<button class="fullwidth button--primary">
@@ -84,13 +91,14 @@
 </template>
 
 <script>
-	import data from "../../data.json";
+	import jsonData from "../../data.json";
 
 	export default {
 		name: 'Home',
 		data: () => ({
-			data: data,
+			data: jsonData,
 			price: 0,
+			userStep: 0,
 			selectedTypes: []
 		}),
 		components: {},
@@ -134,17 +142,37 @@
 						selected: selected,
 					});
 				this.calculate();
-				console.log('this.selectedTypes', this.selectedTypes)
 			},
 
 			isTypeSelected(type, selected) {
 				let isSelected = false;
 				this.selectedTypes.forEach(el => {
-					if (el.type === type && el.selected.title === selected.title) isSelected = true;
+					if (el.type === type && el.selected.title === selected.title)
+						isSelected = true;
 				});
 				return isSelected;
+			},
+		},
+
+		watch: {
+			selectedTypes() {
+				this.userStep = this.selectedTypes.length
 			}
 		},
+
+		created() {
+			this.data.forEach(step => {
+				if (step.variants) {
+					step.variants.forEach(variant => {
+						if (variant.select && variant.select.length) {
+							variant.select.forEach(select => {
+								select.select = select.items[0].price
+							})
+						}
+					})
+				}
+			})
+		}
 	}
 </script>
 
@@ -173,15 +201,15 @@
 				height: 100%;
 				margin: 0;
 				border: none;
-				color: #e5e5e5;
-				background-color: #e5e5e5;
+				color: $gray;
+				background-color: $gray;
 				width: 2px;
 			}
 
 			&__checkbox__block {
 				padding: 8px;
 				border-radius: $border-radius;
-				border: 1px solid #e5e5e5;
+				border: 1px solid $gray;
 				margin: 8px 0;
 				color: #9a9a9a;
 			}
